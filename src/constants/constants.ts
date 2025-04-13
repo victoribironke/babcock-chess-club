04aea5b3ed7fe91a97e369e3ce5fc4f6dbc329db
Victor Ibironke <victoribironke2019@gmail.com>
2025-04-13 14:02:45 +0100

    feat: expand CHESS_COM_PLAYERS and LICHESS_PLAYERS arrays

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -10,9 +10,55 @@ export const PAGES = {
 };
 
 export const CHESS_COM_PLAYERS = Array.from(
-  new Set(["boy_victor"].map((p) => p.toLowerCase().trim()))
+  new Set(
+    [
+      "boy_victor",
+      "Cammizy",
+      "danilawore",
+      "BishopF807",
+      "heisoseni",
+      "planning_guy",
+      "truthwithtemi",
+      "AbbeyTk",
+      "timidavidopeyemi",
+      "tecnosam",
+      "Cheta8",
+      "cityboynati",
+      "fex_og",
+      "Ghostslayer4816",
+      "Slipn_slide",
+      "OlaDaClutcher",
+      "Danny-1400",
+      "Akogo",
+      "Francis171",
+      "dayomisi",
+    ].map((p) => p.toLowerCase().trim())
+  )
 );
 
 export const LICHESS_PLAYERS = Array.from(
-  new Set(["boy_victor"].map((p) => p.toLowerCase().trim()))
+  new Set(
+    [
+      "boy_victor",
+      "jidenna",
+      "cityboynati",
+      "Danny-1400",
+      "simon25",
+      "dayomisi",
+      "Mozeez1",
+      "heisoseni",
+      "Posibright",
+      "Cammizy",
+      "Ozzychuks",
+      "GhostSlayer4816",
+      "knight66999",
+      "Cheta8",
+      "George1603",
+      "Artonex_Ayz",
+      "timidavidopeyemi",
+      "truthwithtemi",
+      "Activeeeee",
+      "DannyLaw",
+    ].map((p) => p.toLowerCase().trim())
+  )
 );
